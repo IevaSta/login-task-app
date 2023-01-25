ade0650982be173793702c829ceda155ffe6a539
IevaSta <ieva@kontraforma.lt>
2023-01-25 20:19:18 +0200

    scss theme, reset, body, font

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
+import { useState } from 'react';
 import './App.scss';
 import GreetingPage from './Components/GreetingPage';
 import LoginPage from './Components/LoginPage';
 
 function App() {
 
-
+  const [loggedIn, setLoggedIn] = useState(true)
 
   return (
-    <div>
-      <LoginPage />
-      <GreetingPage />
-    </div>
+    <main className='container'>
+
+      {loggedIn === true ? <LoginPage /> : <GreetingPage />}
+
+    </main>
   );
 }
 
