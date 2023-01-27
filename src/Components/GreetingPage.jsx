d50d0bf9eb0513797260de84eb6190fca22d94c7
IevaSta <ieva@kontraforma.lt>
2023-01-27 14:39:45 +0200

    inputs icon added

--- src/Components/GreetingPage.jsx
+++ src/Components/GreetingPage.jsx
@@ -1,10 +1,13 @@
 function GreetingPage() {
 
     return (
-        <>
-            <h1 className="main-title">Welcome in the app!</h1>
-            <button>Logout</button>
-        </>
+        <main className='container'>
+            <div className="bin-content">
+                <h1 className="main-title">Welcome in the app!</h1>
+                <button>Logout</button>
+            </div>
+
+        </main>
     )
 }
 
